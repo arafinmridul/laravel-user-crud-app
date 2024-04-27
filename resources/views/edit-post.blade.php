a0c637740dc805bf953acdf6cfe5a5cf9ee8d15d
Arafin Mridul <mridularafin@gmail.com>
2024-04-28 04:38:13 +0600

    styling done

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -5,15 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit Post</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <h1>Edit Post</h1>
     <form action="/edit-post/{{$post->id}}" method="POST">
         @csrf
         @method('PUT')
-        <input type="text" name="title" value="{{$post->title}}">
-        <textarea name="body">{{$post->body}}</textarea>
-        <button>Save Changes</button>
+        <label for="exampleFormControlInput1" class="form-label">Post Title</label>
+        <input type="text" name="title" value="{{$post->title}}" class="form-control mb-2" id="exampleFormControlInput1">
+        <label for="exampleFormControlTextarea1" class="form-label">Edit your post...</label>
+        <textarea name="body" class="form-control mb-2" id="exampleFormControlTextarea1" rows="3">{{$post->body}}</textarea>
+        <button class="btn btn-primary">Save Changes</button>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     @auth
@@ -13,15 +14,17 @@
     </p>
     <form action="/logout" method="POST">
         @csrf
-        <button>Log out</button>
+        <button class="btn btn-secondary ms-3">Log out</button>
     </form>
     <div style="border: 1.5px solid black; padding: 25px">
         <h2>Create a new post</h2>
         <form action="/create-post" method="POST">
             @csrf
-            <input type="text" name="title" placeholder="Post Title">
-            <textarea name="body" placeholder="Write your post..."></textarea>
-            <button>Create Post</button>
+            <label for="exampleFormControlInput1" class="form-label">Post Title</label>
+            <input type="text" name="title" class="form-control" id="exampleFormControlInput1" placeholder="Post Title">
+            <label for="exampleFormControlTextarea1" class="form-label">Write your post...</label>
+            <textarea name="body" placeholder="Write your post..." class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <button class="btn btn-dark">Create Post</button>
         </form>
     </div>
 
@@ -31,11 +34,11 @@
             <div style="background-color:rgb(233, 224, 224); padding:10px; margin:3px">
                 <h3>{{$post['title']}} by {{$post->user->name}}</h3>
                 <p> {{$post['body']}} </p>
-                <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
+                <button class="btn btn-dark mb-2"><a href="/edit-post/{{$post->id}}" class="text-light">Edit</a></button>
                 <form action="/delete-post/{{$post->id}}" method="POST">
                     @csrf
                     @method('DELETE')
-                    <button>Delete</button>
+                    <button class="btn btn-danger">Delete</button>
                 </form>
             </div>
         @endforeach
@@ -48,7 +51,7 @@
             <input type="text" name="name" placeholder="Name">
             <input type="email" name="email" placeholder="Email">
             <input type="password" name="password" placeholder="Password">
-            <button type="submit">Register</button>
+            <button type="submit" class="btn btn-success">Register</button>
         </form>
     </div>
     <div style="border: 1.5px solid black; padding: 25px;">
@@ -57,9 +60,10 @@
             @csrf
             <input type="text" name="loginname" placeholder="Login Name">
             <input type="password" name="loginpassword" placeholder="Password">
-            <button type="submit">Log In</button>
+            <button type="submit" class="btn btn-primary">Log In</button>
         </form>
     </div>
     @endauth
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
